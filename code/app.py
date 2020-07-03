ca2a1000600f30ab4dc6e5bd27a0f2d895db254b
ryoichi.hirai <ryoichi.hirai@mobilus.co.jp>
2020-07-03 14:47:01 +0900

    auto format

--- code/app.py
+++ code/app.py
@@ -1,16 +1,18 @@
+import boto3
+import datetime
+import logging
+import account as ac
+from oandapyV20.exceptions import V20Error
+from oandapyV20.endpoints.pricing import PricingStream
+from oandapyV20 import API
+import json
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'package'))
-import json
-from oandapyV20 import API
-from oandapyV20.endpoints.pricing import PricingStream
-from oandapyV20.exceptions import V20Error
-import account as ac
-import logging
-import datetime
-import boto3
+sys.path.append(os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), 'package'))
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, context):
     environment = event['environment']
@@ -20,10 +22,10 @@ def lambda_handler(event, context):
     message = "OK"
 
     api = API(access_token=ac.access_token, environment=environment)
- 
+
     params = {"instruments": instruments}
     ps = PricingStream(ac.account_number, params)
-    
+
     dynamoDB = boto3.resource("dynamodb")
     table = dynamoDB.Table(instruments)
 
@@ -31,14 +33,14 @@ def lambda_handler(event, context):
         for rsp in api.request(ps):
             if "bids" in rsp.keys():
                 table.put_item(
-                    Item = {
+                    Item={
                         "DATETIME": datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y%m%d%H%M%S'),
                         "bid": rsp["bids"][0]["price"],
                         "ask": rsp["asks"][0]["price"]
                     }
                 )
                 break
- 
+
     except V20Error as e:
         statusCode = 500
         message = "Error: {}".format(e)
